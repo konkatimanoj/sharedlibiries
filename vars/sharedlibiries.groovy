38a0f936698f4ad9d0357e0b72e48675bead3ece
konkatimanoj <162788980+konkatimanoj@users.noreply.github.com>
2024-06-06 15:48:42 +0530

    Update sharedlibiries.groovy

--- vars/sharedlibiries.groovy
+++ vars/sharedlibiries.groovy
@@ -1,10 +1,17 @@
-
 def gitdownload(repo)
 {
-   git 'https://github.com/intelliqittrainings/$(repo).git'
-
+  git 'https://github.com/intelliqittrainings/${repo}.git'
 }
-def gitbuild()
+def build()
 {
-  sh 'mvn package'
+  sh "mvn package"
+}
+def deploy(jobname,ip,appname)
+{
+   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+}
+
+def runselenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
